f3fe9db56f2ec48516e1e088f4d95ad8a0a286f9
chetheaker <che.theaker@gmail.com>
2022-11-11 21:48:07 +0100

    overview section started

--- client/src/Components/TripDetails/TripOverview/TripOverview.js
+++ client/src/Components/TripDetails/TripOverview/TripOverview.js
@@ -1,14 +1,33 @@
 import './TripOverview.css';
-import { FiEdit } from 'react-icons/fi';
+import { GiPathDistance } from 'react-icons/gi';
+import { BiTimeFive } from 'react-icons/bi';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { GrLocation } from 'react-icons/gr';
 
 function TripOverview() {
   return (
     <div id="overview" className="section">
-      OVERVIEW
+      <div className="top">
+        <div className="widget total-distance">
+          <GiPathDistance size="1.25em" />
+          <h1>Trip Distance: 500km</h1>
+        </div>
+        <div className="widget total-time">
+          <BiTimeFive size="1.25em" />
+          <h1>Trip Time: 17hr 45min</h1>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="widget total-days">
+          <AiOutlineCalendar size="1.25em" />
+          <h1>Road Trip Days: 7</h1>
+        </div>
+        <div className="widget total-stops">
+          <GrLocation size="1.25em" />
+          <h1>Road Trip Stops: 4</h1>
+        </div>
+      </div>
       <div className="overlay"></div>
-      <button className="icon-container">
-        <FiEdit color="#fff" size="1.5em" />
-      </button>
     </div>
   );
 }
