6af99e4794c0469e0b4dccc3d64a083e93e87cc9
chetheaker <che.theaker@gmail.com>
2022-11-13 14:30:53 +0100

    glassmorphism added to trpi nav

--- client/src/Components/TripDetails/TripDetailsNav/TripDetailsNav.js
+++ client/src/Components/TripDetails/TripDetailsNav/TripDetailsNav.js
@@ -1,12 +1,35 @@
 import './TripDetailsNav.css';
+import { useState } from 'react';
 
 function TripDetailsNav() {
+  const [active, setActive] = useState('overview');
+
   return (
     <div id="trip-nav">
-      <button>Overview</button>
-      <button>Route</button>
-      <button>Itinerary</button>
-      <button>Checklist</button>
+      <button
+        onClick={() => setActive('overview')}
+        className={`${active === 'overview' ? 'active' : ''}`}
+      >
+        Overview
+      </button>
+      <button
+        onClick={() => setActive('route')}
+        className={`${active === 'route' ? 'active' : ''}`}
+      >
+        Route
+      </button>
+      <button
+        onClick={() => setActive('suggestions')}
+        className={`${active === 'suggestions' ? 'active' : ''}`}
+      >
+        For You
+      </button>
+      <button
+        onClick={() => setActive('itinerary')}
+        className={`${active === 'itinerary' ? 'active' : ''}`}
+      >
+        Itinerary
+      </button>
     </div>
   );
 }
