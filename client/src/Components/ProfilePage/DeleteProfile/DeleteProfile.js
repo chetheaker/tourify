65cb94048113046a8e4cc0fcef05dbeb90b65985
chetheaker <che.theaker@gmail.com>
2022-11-14 10:00:21 +0100

    logout updated

---
+++ client/src/Components/ProfilePage/DeleteProfile/DeleteProfile.js
@@ -0,0 +1,61 @@
+import './DeleteProfile.css';
+import { useRef } from 'react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure
+} from '@chakra-ui/react';
+
+function DeleteProfile({ id, handleLogOut }) {
+  const cancelRef = useRef();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const handleProfileDelete = async () => {
+    console.log(id);
+    // await handleLogOut();
+    onClose();
+  };
+
+  return (
+    <>
+      <div className="delete-profile">
+        <Button colorScheme="red" onClick={onOpen}>
+          Delete Profile
+        </Button>
+      </div>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Profile
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={handleProfileDelete} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
+
+export default DeleteProfile;
--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -1,14 +1,27 @@
 import './ProfilePage.css';
 import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import UserContext from '../../UserContext';
 import { logoutUser } from '../../Utils/UserService';
 import NavBar from '../NavBar/NavBar';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure
+} from '@chakra-ui/react';
+import DeleteProfile from './DeleteProfile/DeleteProfile';
 
 function ProfilePage() {
   // eslint-disable-next-line
-  const [_, setActiveUser] = useContext(UserContext);
+  const [activeUser, setActiveUser] = useContext(UserContext);
   const navigate = useNavigate();
+  const cancelRef = useRef();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   const handleLogOut = async () => {
     await logoutUser();
@@ -20,8 +33,43 @@ function ProfilePage() {
     <>
       <NavBar />
       <div className="container">
-        <h1>PROFILE</h1>
-        <button onClick={handleLogOut}>LOGOUT</button>
+        <div className="profile-container">
+          <div className="profile-heading">
+            <h1>{activeUser.first_name}'s Profile</h1>
+            <div className="right">
+              <Button className="logout" onClick={onOpen} bg="white">
+                LOGOUT
+              </Button>
+              <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+              >
+                <AlertDialogOverlay>
+                  <AlertDialogContent>
+                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                      Log Out
+                    </AlertDialogHeader>
+
+                    <AlertDialogBody>
+                      Are you sure you want to log out?
+                    </AlertDialogBody>
+
+                    <AlertDialogFooter>
+                      <Button ref={cancelRef} onClick={onClose}>
+                        Cancel
+                      </Button>
+                      <Button colorScheme="red" onClick={handleLogOut} ml={3}>
+                        Log Out
+                      </Button>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialogOverlay>
+              </AlertDialog>
+              <DeleteProfile handleLogOut={handleLogOut} id={activeUser._id} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
