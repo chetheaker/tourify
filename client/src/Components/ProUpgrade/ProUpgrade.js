a66d592c078c1bc9d18d40759c3361ff26352769
chetheaker <che.theaker@gmail.com>
2022-11-15 10:42:47 +0100

    tourify pro banner made

--- client/src/Components/ProUpgrade/ProUpgrade.js
+++ client/src/Components/ProUpgrade/ProUpgrade.js
@@ -1,5 +1,6 @@
 import './ProUpgrade.css';
 import { Button } from '@chakra-ui/react';
+import { TiTick } from 'react-icons/ti';
 
 function ProUpgrade() {
   return (
@@ -11,6 +12,26 @@ function ProUpgrade() {
         </div>
         <div className="right">
           <Button colorScheme="green">UPGRADE</Button>
+        </div>
+      </div>
+      <div className="body">
+        <div className="left">
+          <h1 className="price">$9.99</h1>
+          <h1 className="month">per month</h1>
+        </div>
+        <div className="right">
+          <div className="feature">
+            <TiTick size="1.5em" color="#1FC28B" />
+            <h1>Unlimited Road Trips</h1>
+          </div>
+          <div className="feature">
+            <TiTick size="1.5em" color="#1FC28B" />
+            <h1>Customised Suggestions</h1>
+          </div>
+          <div className="feature">
+            <TiTick size="1.5em" color="#1FC28B" />
+            <h1>No Ads</h1>
+          </div>
         </div>
       </div>
     </div>
